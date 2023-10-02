da49aa86a05341916f751e91ab99aca79a98f891
Bleaze Perris <bperriscode@gmail.com>
2023-10-02 15:15:35 -0400

    added button functionality for main header

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import './style.css'
+import {displayHeading} from './setup'
 
-function clearPage() {
-    const element = document.querySelector('#content')
+function loadMainButtons() {
 
-    element.textContent = ''
+    const inbox = document.querySelector('#inbox')
+    inbox.addEventListener('click', () => {
+        displayHeading(inbox.textContent)
+    })
+
+    const today = document.querySelector('#today')
+    today.addEventListener('click', () => {
+        displayHeading(today.textContent)
+    })
+
+    const this_week = document.querySelector('#this-week')
+    this_week.addEventListener('click', () => {
+        displayHeading(this_week.textContent)
+    })
 }
 
-function loadPage() {
-
-}
-
-loadPage();
+loadMainButtons()
---
+++ src/setup.js
@@ -0,0 +1,8 @@
+function displayHeading(title) {
+    const element = document.querySelector('.main-heading')
+    element.textContent = title
+}
+
+export {
+    displayHeading
+}
